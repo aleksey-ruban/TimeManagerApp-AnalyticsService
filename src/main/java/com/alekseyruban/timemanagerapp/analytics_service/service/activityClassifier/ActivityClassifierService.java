b303a04505e8d57243ad596fd3dc593aecfeb606
aleksey-ruban <alekseyruban555@gmail.com>
2026-03-28 02:34:44 +0300

    Fix resource files using

--- src/main/java/com/alekseyruban/timemanagerapp/analytics_service/service/activityClassifier/ActivityClassifierService.java
+++ src/main/java/com/alekseyruban/timemanagerapp/analytics_service/service/activityClassifier/ActivityClassifierService.java
@@ -8,26 +8,47 @@ import com.alekseyruban.timemanagerapp.analytics_service.service.activityClassif
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ActivityClassifierService {
 
-    private final String tokenizerPath = "src/main/resources/aiModels/multilingualE5base/tokenizer.json";
-    private final String modelPath = "src/main/resources/aiModels/multilingualE5base/model_O4.onnx";
-    private final String classifierPath = "src/main/resources/aiModels/activityClassifier/activity_classifier.onnx";
-
     private final TextEmbedder embedder;
     private final ActivityClassifier classifier;
 
     public ActivityClassifierService() {
         try {
+            String tokenizerPath = copyResourceToTemp("aiModels/multilingualE5base/tokenizer.json");
+            String modelPath = copyResourceToTemp("aiModels/multilingualE5base/model_O4.onnx");
+            String classifierPath = copyResourceToTemp("aiModels/activityClassifier/activity_classifier.onnx");
+
             embedder = new TextEmbedder(tokenizerPath, modelPath);
             classifier = new ActivityClassifier(classifierPath);
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String copyResourceToTemp(String resourcePath) throws IOException {
+        InputStream is = getClass().getClassLoader().getResourceAsStream(resourcePath);
+        if (is == null) {
+            throw new FileNotFoundException("Resource not found: " + resourcePath);
+        }
+
+        String suffix = resourcePath.contains(".") ? resourcePath.substring(resourcePath.lastIndexOf('.')) : null;
+        Path tempFile = Files.createTempFile("temp-", suffix);
+        tempFile.toFile().deleteOnExit(); // удалим при выходе
+
+        Files.copy(is, tempFile, StandardCopyOption.REPLACE_EXISTING);
+        return tempFile.toAbsolutePath().toString();
     }
 
     public ActivityClass classifyActivity(ActivitySnapshotDto activity, CategorySnapshotDto category) {
